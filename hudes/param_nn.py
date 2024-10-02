4efcd77fa40f1dc8cfad77fccf57798cbddf5a53
Misko <misko@cs.toronto.edu>
2024-10-01 19:57:06 -0700

    add tests and mnist equivalent

--- hudes/param_nn.py
+++ hudes/param_nn.py
@@ -1,6 +1,20 @@
 import torch
 
 
+class Sequential:
+    def __init__(self, modules_list):
+        self.params = 0
+        self.modules_list = modules_list
+
+    def forward(self, models_params, x):
+        for module in self.modules_list:
+            if isinstance(module, torch.nn.Module):
+                x = module(x)
+            else:
+                models_params, x = module.forward(models_params, x)
+        return models_params, x
+
+
 class Linear:
     def __init__(self, input_channels, output_channels):
         self.input_channels = input_channels
@@ -19,7 +33,8 @@ class Linear:
             :, self.weight_params : self.weight_params + self.bias_params
         ].reshape(-1, 1, self.bias_params)
         # x ~ (batch,input_dim)
+        output = torch.einsum("moi,mbi->mbo", _weights, x) + _bias
         return (
             models_params[:, self.params :],
-            torch.einsum("moi,mbi->mbo", _weights, x) + _bias,
+            output,
         )
--- tests/test_param_nn.py
+++ tests/test_param_nn.py
@@ -1,9 +1,9 @@
 import torch
 
-from hudes.param_nn import Linear
+from hudes.param_nn import Linear, Sequential
 
 
-def test_linear():
+def test_linear_and_relu():
 
     in_channels = 3
     out_channels = 5
@@ -16,6 +16,7 @@ def test_linear():
     )
 
     _mp, out = Linear(in_channels, out_channels).forward(model_params, batch)
+    out = torch.nn.functional.relu(out)
 
     outputs = []
     for model_idx in range(n_models):
@@ -26,6 +27,46 @@ def test_linear():
         layer = torch.nn.Linear(in_channels, out_channels)
         layer.weight.data.copy_(_weight)
         layer.bias.data.copy_(_bias)
-        outputs.append(layer(batch[0]).unsqueeze(0))
+        outputs.append(torch.nn.functional.relu(layer(batch[0])).unsqueeze(0))
 
     assert torch.vstack(outputs).isclose(out).all()
+
+
+def test_mnist():
+    mnist_width_height = 28
+    mnist_classes = 10
+    hidden = 32
+    mnist_net = torch.nn.Sequential(
+        torch.nn.Flatten(),
+        torch.nn.Linear(mnist_width_height * mnist_width_height, hidden),
+        torch.nn.ReLU(),
+        torch.nn.Linear(hidden, hidden),
+        torch.nn.ReLU(),
+        torch.nn.Linear(hidden, mnist_classes),
+        torch.nn.LogSoftmax(dim=1),
+    )
+
+    mnist_param_net = Sequential(
+        [
+            torch.nn.Flatten(start_dim=2),
+            Linear(mnist_width_height * mnist_width_height, hidden),
+            torch.nn.ReLU(),
+            Linear(hidden, hidden),
+            torch.nn.ReLU(),
+            Linear(hidden, mnist_classes),
+            torch.nn.LogSoftmax(dim=2),
+        ]
+    )
+
+    params = torch.hstack(
+        [p.clone().reshape(-1) for p in mnist_net.parameters()]
+    ).reshape(1, -1)
+    params = torch.vstack([params, params + 0.01, params])
+
+    batch = torch.rand(1, 7, mnist_width_height, mnist_width_height)
+
+    out = mnist_param_net.forward(params, batch)
+    _out = mnist_net(batch[0])
+    assert out[1][0].isclose(_out, atol=1e-5).all()
+    assert not out[1][1].isclose(_out, atol=1e-5).all()
+    assert out[1][2].isclose(_out, atol=1e-5).all()
