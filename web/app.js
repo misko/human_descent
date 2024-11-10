03ddc165ac83034dd64e6855fb8102266822cec1
Misko <misko@cs.toronto.edu>
2024-11-10 14:49:49 -0800

    move to cdn

--- web/app.js
+++ web/app.js
@@ -8,7 +8,8 @@ import KeyboardClientGL from './client/KeyboardClientGL.js';
 
 // Initialize the client
 //const client = new KeyboardClient('localhost', 8765);
-const client = new KeyboardClientGL('localhost', 8765);
+//const client = new KeyboardClientGL('localhost', 8765);
+const client = new KeyboardClientGL('104.171.202.30', 8765);
 
 
 client.runLoop();
